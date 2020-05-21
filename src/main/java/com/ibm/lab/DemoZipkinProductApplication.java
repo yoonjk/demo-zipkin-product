9db3ce29b9dfad87da36673d85717b4a57831001
nexweb1 <nexweb1@gmail.com>
2020-05-21 14:32:16 +0900

    Tracing demo order to call product service using zipkin

---
+++ src/main/java/com/ibm/lab/DemoZipkinProductApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoZipkinProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoZipkinProductApplication.class, args);
+	}
+
+}
